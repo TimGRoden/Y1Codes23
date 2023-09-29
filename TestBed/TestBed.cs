f842a0f8a23c483b1d24e5078d5a64c2913a1d26
Timothy Roden <tgr@barton.ac.uk>
2023-09-29 14:16:52 +0100

    Q1 and 2 of the W104 While

--- TestBed/TestBed.cs
+++ TestBed/TestBed.cs
@@ -11,12 +11,8 @@ namespace TestBed
 
         static void Main(string[] args)
         {
-            Console.WriteLine("How many rows do you want?");
-            int rows = int.Parse(Console.ReadLine());
-            for (int i = 0; i < rows; i++)
-            {
-                Console.WriteLine(new string(' ', rows - i - 1) + new string('*', (i + 1) * 2));
-            }
+            
+
 
             Console.ReadKey();
         }
---
+++ W104 While/W104.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace W104_While
+{
+    internal class W104
+    {
+        static void Main(string[] args)
+        {
+            switch (Console.ReadLine())
+            {
+                case "1":
+                    {
+                        int x = 0;
+
+                        do //Do the code once, before the conditional check.
+                        {
+                            Console.Write("Enter number: ");
+                            x = int.Parse(Console.ReadLine());
+                            Console.WriteLine(x);
+                        } while (x > 0);
+
+                        Console.WriteLine("DONE");
+                        Console.ReadKey();
+
+
+                        x = 0;
+
+                        while (x > 0) //Just check x at the start, as it's already 0.
+                        {
+                            Console.Write("Enter number: ");
+                            x = int.Parse(Console.ReadLine());
+                            Console.WriteLine(x);
+                        }
+
+                        Console.WriteLine("DONE");
+                        Console.ReadKey();
+                    }break;
+                case "2":
+                    {
+                        int attempts = 0;
+                        string realPin = "1123";
+                        while (attempts < 3)
+                        {
+                            Console.WriteLine("Please enter a pin.");
+                            string userPin = Console.ReadLine();
+                            if (userPin == realPin)
+                            {
+                                Console.WriteLine("That's right! Welcome.");
+                                break;
+                            }
+                            Console.WriteLine("That wasn't right. Please try again.");
+                            Console.WriteLine($"You have {3 - attempts} attempts left.");
+                            attempts++;
+                        }
+                    }break;
+            }
+
+
+
+        }
+    }
+}
