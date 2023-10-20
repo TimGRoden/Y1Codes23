5de79345f0de18278fd8aa4c6e5506c6b47d266e
Timothy Roden <tgr@barton.ac.uk>
2023-10-20 14:37:02 +0100

    Completed W104 basic tasks

---
+++ A104/A104.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A104
+{
+    internal class A104
+    {
+        static Random rnd = new Random();
+        static void Q1()
+        {
+            Console.WriteLine("Welcome to the n sided die.\n");
+            Console.WriteLine("How many sides would you like your die to have?");
+            int uSides = int.Parse(Console.ReadLine());
+            Console.WriteLine("\nHow many times would you like to roll your die?");
+            int uRolls = int.Parse(Console.ReadLine());
+            Console.WriteLine($"\nhere are your rolls:");
+            Console.WriteLine(roll(uSides, uRolls));
+        }
+        static void Q2()
+        {
+            Console.WriteLine("Welcome to Sum or Factorial\n");
+            Console.WriteLine("Enter a number");
+            int userNum = int.Parse(Console.ReadLine());
+            Console.WriteLine("Would you like to sum all numbers up to 5 or find the factorial of the number?");
+            switch (getUserChoice())
+            {
+                case 's': Console.WriteLine($"Sum of all numbers up to {userNum} = {calcSum(userNum)}"); break;
+                case 'f': Console.WriteLine($"Factorial of {userNum} = {calcFact(userNum)}"); break;
+            }
+            
+            Console.WriteLine("bye!");
+        }
+        static char getUserChoice()
+        {
+            char result;
+            do 
+            {
+                Console.WriteLine("Enter s for sum and f for factorial"); 
+                result = Console.ReadKey().KeyChar; 
+            } while (result != 's' && result != 'f');
+            return result;
+        }
+        static int calcSum(int n) => n == 0 ? 0 : (n + calcSum(n - 1));
+        static int calcFact(int n) => n == 1 ? 1 : (n * calcFact(n - 1));
+        static string roll(int sides, int n) => n == 0 ? "\n" : $"{rnd.Next(1,sides+1)}\n{roll(sides, n-1)}";
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Which question? (1/2)");
+            char userChoice = Console.ReadKey(true).KeyChar;
+            Console.Clear();
+            switch (userChoice)
+            {
+                case '1': Q1(); break;
+                default: Q2(); break;
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ W104 - While/W104.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace W104___While
+{
+    internal class W104
+    {
+        static void Q1()
+        {
+            Console.Clear(); Console.WriteLine("1. Basics");
+            int x = 0;
+
+            while (x>0) //As x starts at 0, this loop works immediately like this.
+            {
+                Console.Write("Enter number: ");
+                x = int.Parse(Console.ReadLine());
+                Console.WriteLine(x);
+            }
+
+            Console.WriteLine("DONE");
+            Console.ReadKey();
+
+        }
+        static void Q2()
+        {
+            string truePin = "11235813"; //The fibonacci numbers!
+            int attempts = 0;
+            Console.Clear(); Console.WriteLine("2. Cash Machine");
+            while (attempts != 3)
+            {
+                Console.WriteLine("Please enter your 8 digit PIN:");
+                string userPin = Console.ReadLine();
+                if (userPin == truePin)
+                {
+                    Console.WriteLine("Welcome to the C# Bank."); break;
+                } else
+                {
+                    attempts ++;
+                    if (attempts < 3) Console.WriteLine($"Incorrect pin, you have {3 - attempts} attempts left.");
+                    else Console.WriteLine("Incorrect pin. You have been locked out of this account.");
+                }
+            }
+
+        }
+        static void Q3()
+        {
+            Console.Clear(); Console.WriteLine("3. Class Counter");
+            Console.WriteLine("Please enter members of the class one at a time. Enter '.' when finished.");
+            int items = -1;
+            string lastItem;
+            do
+            {
+                items++;
+                Console.Write($"{items + 1}. "); //Makes a nice ordered list.
+                lastItem = Console.ReadLine();
+            } while (lastItem != ".");
+            Console.CursorTop--; //Overwrites the '.' row.
+            Console.WriteLine($"Completed class list. You have entered {items} names.");
+
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Which question do you want to do?");
+            Console.WriteLine("1. Basics");
+            Console.WriteLine("2. Cash Machine");
+            Console.WriteLine("3. Class Counter");
+            switch (Console.ReadKey().KeyChar)
+            {
+                case '1': Q1(); break;
+                case '2': Q2(); break;
+                default: Q3(); break;
+            }
+            Console.ReadKey();
+        }
+    }
+}
